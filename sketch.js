306dd3749966bd3190e75409d94ebb845acb2674
Joel Noyce <noyce.joel@gmail.com>
2024-02-05 20:00:14 +0000

    grid setup and basic simulation implemented

--- sketch.js
+++ sketch.js
@@ -1,53 +1,79 @@
 const make2DArray = (rows, cols) => {
   const arr = new Array(rows);
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(cols).fill(0)
-  }
-  return arr;
-}
-
-let grid;
-let w = 5;
-let cols, rows;
-
-
-function setup() {
-  createCanvas(400, 400);
-  cols = width / w;
-  rows = height / w;
-  grid = make2DArray(rows, cols)
-  
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      grid[i][j] = 0
+    arr[i] = new Array(cols);
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = 0;
     }
   }
-  grid[20][10] = 100
+  return arr;
+};
+
+let grid;
+let w = 20;
+let cols, rows;
+
+function setup() {
+  createCanvas(500, 500);
+  cols = width / w;
+  rows = height / w;
+  grid = make2DArray(rows, cols);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      grid[i][j] = 0;
+    }
+  }
+  
+}
+
+function mouseDragged() {
+  let row = floor(mouseX / w);
+  let col = floor(mouseY / w);
+  if (col >=0 && col < cols - 1 && row >= 0 && row <= rows - 1) {
+    grid[row][col] = 1;
+  }
+  
 }
 
 function draw() {
-  background(100);
+  background(0)
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      stroke(255)
-      fill(grid[i][j] * 255)
-      let x = i * w;
-      let y = j * w;
-      square(x, y, w)
+      noStroke();
+      if (grid[i][j] === 1) {
+        fill(255);
+        let x = i * w;
+        let y = j * w;
+        square(x, y, w);
+      }
+      
     }
   }
-  
+
   let nextGrid = make2DArray(rows, cols);
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       let state = grid[i][j];
       if (state === 1) {
         let below = grid[i][j + 1];
+        let dir = random([-1, 1]);
+        let belowLeft, belowRight;
+        if (i > 0 && i < rows - 1 && j < cols - 1) {
+          belowLeft = grid[i - dir][j + 1];
+          belowRight = grid[i + dir][j + 1];
+        }
         if (below === 0) {
-          
+          nextGrid[i][j + 1] = 1;
+        } else if (belowRight === 0) {
+          nextGrid[i + dir][j + 1] = 1;
+        } else if (belowLeft === 0) {
+          nextGrid[i - dir][j + 1] = 1;
+        } else {
+          nextGrid[i][j] = 1;
         }
       }
     }
   }
-  
+  grid = nextGrid;
 }
